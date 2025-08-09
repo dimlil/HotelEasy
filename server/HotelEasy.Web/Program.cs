d92c065f48dacf618ee19eeb4eb7db91637f6dcc
dimlil <d.v.vassilev@gmail.com>
2025-08-10 01:35:48 +0300

    feat/ added env and config the logs

--- server/HotelEasy.Web/Program.cs
+++ server/HotelEasy.Web/Program.cs
@@ -1,10 +1,23 @@
+using dotenv.net;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotEnv.Load();
+var envVars = DotEnv.Read();
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString ?? "";
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 var app = builder.Build();
 
