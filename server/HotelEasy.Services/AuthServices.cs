0f6375f576d1bfb51d24e857d03e0d7a6451b54f
dimlil <d.v.vassilev@gmail.com>
2025-08-19 21:44:13 +0300

    feat/ added basic login

--- server/HotelEasy.Services/AuthServices.cs
+++ server/HotelEasy.Services/AuthServices.cs
@@ -1,4 +1,7 @@
 using HotelEasy.Data;
+using HotelEasy.Services.DTO;
+using Microsoft.EntityFrameworkCore;
+using HotelEasy.Services.Common;
 
 namespace HotelEasy.Services
 {
@@ -9,6 +12,14 @@ namespace HotelEasy.Services
         public AuthServices(Diplomna21180105Context context)
         {
             _context = context;
+        }
+
+        public async Task<ServiceResult<string>> LoginAsync(UserLoginDTO UserLoginDTO)
+        {
+            System.Console.WriteLine("Attempting to login user: " + UserLoginDTO.Email);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == UserLoginDTO.Email);
+            
+            return ServiceResult<string>.SuccessResult(user.Email);
         }
     }
 }
