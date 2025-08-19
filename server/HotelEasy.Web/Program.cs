e9eb42e663cdb8ce4a49c40d73017f4b3e6ff4e2
dimlil <d.v.vassilev@gmail.com>
2025-08-19 21:43:03 +0300

    fix/ env for connection string

--- server/HotelEasy.Web/Program.cs
+++ server/HotelEasy.Web/Program.cs
@@ -1,42 +1,33 @@
 using dotenv.net;
 using Microsoft.EntityFrameworkCore;
 using HotelEasy.Data;
+using HotelEasy.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 DotEnv.Load();
-var envVars = DotEnv.Read();
-
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
-builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString ?? "";
 
-
-// Add services to the container.
+builder.Services.AddDbContext<Diplomna21180105Context>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
-builder.Services.AddDbContext<Diplomna21180105Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+// Register services
+builder.Services.AddScoped<AuthServices>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
