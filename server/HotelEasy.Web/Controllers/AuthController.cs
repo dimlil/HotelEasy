1c0e8e2f0fbea799ce873e06ce678d103b057f20
dimlil <d.v.vassilev@gmail.com>
2025-08-19 21:45:28 +0300

    feat/ added auth controler

--- server/HotelEasy.Web/Controllers/AuthController.cs
+++ server/HotelEasy.Web/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using HotelEasy.Services;
+using Microsoft.AspNetCore.Identity.Data;
+using HotelEasy.Services.DTO;
 
 namespace ViaPlan.Web.Controllers
 {
@@ -13,5 +15,13 @@ namespace ViaPlan.Web.Controllers
         {
             _authService = authService;
         }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+            return result.Success ? Ok(result.Data) : BadRequest(result.ErrorMessage);
+        }
     }
+
 }
