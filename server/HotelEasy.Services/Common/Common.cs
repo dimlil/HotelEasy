44825a1c30bfd5a1a1d7c895dfd8828ac1f585c2
dimlil <d.v.vassilev@gmail.com>
2025-08-19 21:27:02 +0300

    feat/ added generic ServiceResult<T> wrapper for service responses

---
+++ server/HotelEasy.Services/Common/Common.cs
@@ -0,0 +1,19 @@
+namespace HotelEasy.Services.Common
+{
+    public class ServiceResult<T>
+    {
+        public bool Success { get; set; }
+        public string? ErrorMessage { get; set; }
+        public T? Data { get; set; }
+        public static ServiceResult<T> SuccessResult(T data) => new ServiceResult<T>
+        {
+            Success = true,
+            Data = data
+        };
+        public static ServiceResult<T> Failure(string errorMessage) => new ServiceResult<T>
+        {
+            Success = false,
+            ErrorMessage = errorMessage
+        };
+    }
+}
