7026fd84761830a19c60cec8ce1e3078e1e71a74
dimlil <d.v.vassilev@gmail.com>
2025-08-19 21:59:39 +0300

    feat/ added register

--- server/HotelEasy.Services/AuthServices.cs
+++ server/HotelEasy.Services/AuthServices.cs
@@ -2,6 +2,7 @@ using HotelEasy.Data;
 using HotelEasy.Services.DTO;
 using Microsoft.EntityFrameworkCore;
 using HotelEasy.Services.Common;
+using HotelEasy.Entities;
 
 namespace HotelEasy.Services
 {
@@ -14,11 +15,32 @@ namespace HotelEasy.Services
             _context = context;
         }
 
+        public async Task<ServiceResult<string>> RegisterUserAsync(UserRegisterDTO registerDto)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == registerDto.Email))
+                return ServiceResult<string>.Failure("Username already exists");
+
+            var user = new User
+            {
+                Email = registerDto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(registerDto.PasswordHash),
+                Role = "User"
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return ServiceResult<string>.SuccessResult(user.Email);
+        }
+
         public async Task<ServiceResult<string>> LoginAsync(UserLoginDTO UserLoginDTO)
         {
             System.Console.WriteLine("Attempting to login user: " + UserLoginDTO.Email);
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == UserLoginDTO.Email);
-            
+            // if (user == null || !BCrypt.Net.BCrypt.Verify(UserLoginDTO.Password, user.PasswordHash))
+            //     return ServiceResult<string>.Failure("Invalid username or password");
+
+            // // var token = GenerateJwtToken(user);
             return ServiceResult<string>.SuccessResult(user.Email);
         }
     }
--- server/HotelEasy.Web/Controllers/AuthController.cs
+++ server/HotelEasy.Web/Controllers/AuthController.cs
@@ -16,6 +16,13 @@ namespace ViaPlan.Web.Controllers
             _authService = authService;
         }
 
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO dto)
+        {
+            var result = await _authService.RegisterUserAsync(dto);
+            return result.Success ? Ok(result.Data) : BadRequest(result.ErrorMessage);
+        }
+
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] UserLoginDTO dto)
         {
