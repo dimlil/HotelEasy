b3495a5bfed52cd1379902d7ea54123ce52b5f94
dimlil <d.v.vassilev@gmail.com>
2025-08-22 00:16:33 +0300

    fix/ error message]

--- server/HotelEasy.Services/RoomsServices.cs
+++ server/HotelEasy.Services/RoomsServices.cs
@@ -63,7 +63,7 @@ public class RoomsServices
             var hotel = await _context.Hotels.FindAsync(dto.HotelId);
 
             if (hotel == null)
-                return ServiceResult<RoomDTO>.Failure("User not found");
+                return ServiceResult<RoomDTO>.Failure("Room not found");
 
             room.HotelId = hotel.HotelId;
             room.Hotel = hotel;
@@ -77,4 +77,6 @@ public class RoomsServices
             return new ServiceResult<RoomDTO> { Success = false, ErrorMessage = ex.Message };
         }
     }
+
+    
 }
