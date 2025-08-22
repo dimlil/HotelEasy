0735a911f7c3ca17ea6fd8e170c48fc24133a1a5
dimlil <d.v.vassilev@gmail.com>
2025-08-22 23:39:59 +0300

    feat/ remove ids from dto

--- server/HotelEasy.Services/DTO/ReservationDTO.cs
+++ server/HotelEasy.Services/DTO/ReservationDTO.cs
@@ -2,10 +2,6 @@ namespace HotelEasy.Services.DTO
 {
     public class ReservationDTO
     {
-        public int UserId { get; set; }
-
-        public int RoomId { get; set; }
-
         public DateOnly CheckInDate { get; set; }
 
         public DateOnly CheckOutDate { get; set; }
