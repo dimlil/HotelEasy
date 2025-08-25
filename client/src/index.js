a2508e38986cee024cfad0c9b6ced88a850d2f20
dimlil <d.v.vassilev@gmail.com>
2025-08-26 00:04:57 +0300

    feat/ added react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Navigation from './Navigation.jsx';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
+      <Navigation />
+    </Router>
   </React.StrictMode>
 );
 
