7feae7a794ef1c5eb6a42816c96cea4b75c32c4d
dimlil <d.v.vassilev@gmail.com>
2025-08-27 23:33:12 +0300

    feat/ added env

--- client/src/services/loginUser.js
+++ client/src/services/loginUser.js
@@ -3,9 +3,9 @@ export const loginUser = async (formData) => {
     if (formData.email === '' || formData.password === '') {
         return "All fields must be filled"
     }
-
+    console.log(process.env.REACT_APP_API_URL);
     try {
-        const response = await axios.post('http://localhost:5276/api/auth/login', {
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
             email: formData.email,
             password: formData.password
         }, { withCredentials: true });
