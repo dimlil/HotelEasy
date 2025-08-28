2f57fe3567d3f4d944c99a8591015a9f8e52aa07
dimlil <d.v.vassilev@gmail.com>
2025-08-29 00:24:11 +0300

    fix/ registration

--- client/src/services/registerUser.js
+++ client/src/services/registerUser.js
@@ -6,7 +6,7 @@ export const registerUser = async (formData) => {
 
     try {
         const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/register`, {
-            email: formData.username,
+            email: formData.email,
             password: formData.password
         }, { withCredentials: true });
 
