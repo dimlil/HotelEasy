bba0c5d19b223b89b08aecce50181af0ca5e63b3
dimlil <d.v.vassilev@gmail.com>
2025-08-29 00:57:02 +0300

    refactor

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,14 +1,31 @@
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
-export function getUserRole() {
+export function decodeUserToken() {
+  // const token = localStorage.getItem("userToken");
+  // if (!token) return null;
+
+  // try {
+  //   const decoded = jwtDecode(token);
+  //   return decoded;
+  // } catch (error) {
+  //   return null;
+  // }
+
   const token = localStorage.getItem("userToken");
-  if (!token) return null;
+  if (!token) {
+    return null;
+  }
 
   try {
     const decoded = jwtDecode(token);
-    
-    return decoded.role;
-  } catch (error) {
+    return {
+      role: decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"],
+      email: decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"],
+      id: decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"],
+    };
+  } catch (err) {
+    console.error("Invalid token:", err);
+    localStorage.removeItem("token");
     return null;
   }
 }
