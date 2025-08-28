1f8590b04d017793da99155eb0297704ffe91cdd
dimlil <d.v.vassilev@gmail.com>
2025-08-28 11:03:35 +0300

    remove console.log

--- client/src/services/loginUser.js
+++ client/src/services/loginUser.js
@@ -3,7 +3,6 @@ export const loginUser = async (formData) => {
     if (formData.email === '' || formData.password === '') {
         return "All fields must be filled"
     }
-    console.log(process.env.REACT_APP_API_URL);
     try {
         const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
             email: formData.email,
