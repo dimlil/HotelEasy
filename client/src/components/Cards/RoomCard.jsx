5b4373460b0ec39ddd3efb9e4dd291df17ee0fdd
dimlil <d.v.vassilev@gmail.com>
2025-09-01 14:34:42 +0300

    feat/ added showing rooms

---
+++ client/src/components/Cards/RoomCard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from './hotelCard.module.css'
+
+
+export default function RoomCard(props) {
+    return (
+        <div className={styles.box}>
+            <h2>
+                {props.name}
+            </h2>
+            <p>
+                Цена: {props.price} лв.
+            </p>
+            <Link to={`/rooms/${props.id}`}>Виж Още</Link>
+        </div>
+    )
+}
--- client/src/pages/hotels/HotelDetailsPage.jsx
+++ client/src/pages/hotels/HotelDetailsPage.jsx
@@ -1,29 +1,43 @@
 import { useEffect, useState } from "react"
 import { getHotelById } from "../../services/getHotelById.js"
 import { useParams } from "react-router-dom";
+import styles from './HotelsPage.module.css'
+import RoomCard from "../../components/Cards/RoomCard.jsx";
 
 export default function HotelDetailsPage() {
     const [hotel, setHotel] = useState({})
     const { id } = useParams();
     useEffect(() => {
         const fetchHotels = async () => {
-            setHotel(await getHotelById(id) )
+            setHotel(await getHotelById(id))
         }
         fetchHotels()
-        
+
     }, [id])
     return (
-        <section>
-            {hotel.hotelImages && hotel.hotelImages.map((x, i) => (
-                <img key={i} src={x.url} alt={`Hotel Image ${i + 1}`} />
-            ))}
-            
-            <h2>
-                {hotel.name}
-            </h2>
-            <p>
-                {hotel.location}
-            </p>
+        <section className={styles.hotelDetailsWrapper}>
+            <div className={styles.hotelDetailsHeader}>
+                {hotel.hotelImages && hotel.hotelImages.map((x, i) => (
+                    <img key={i} src={x.url} alt={`Hotel Image ${i + 1}`} />
+                ))}
+
+                <h2>
+                    {hotel.name}
+                </h2>
+                <p>
+                    {hotel.location}
+                </p>
+            </div>
+            <div >
+                <h3>
+                    Стай:
+                </h3>
+                <div className={styles.boxesWrapper}>
+                    {hotel.rooms && hotel.rooms.map((x, i) => (
+                        <RoomCard key={i} id={x._id} name={x.roomNumber} price={x.price} />
+                    ))}
+                </div>
+            </div>
         </section>
     )
 }
