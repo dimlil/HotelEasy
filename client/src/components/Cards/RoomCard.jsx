1ac8f188e4f99c64e4001a83db3bda96e6c25821
dimlil <d.v.vassilev@gmail.com>
2025-09-05 14:43:54 +0300

    fix/ link

--- client/src/components/Cards/RoomCard.jsx
+++ client/src/components/Cards/RoomCard.jsx
@@ -11,7 +11,13 @@ export default function RoomCard(props) {
             <p>
                 Цена: {props.price} лв.
             </p>
-            <Link to={`/rooms/${props.id}?checkIn=${props.checkIn}&checkOut=${props.checkOut}`}>Виж Още</Link>
+            {
+                props.checkIn && props.checkOut ? (
+                    <Link to={`/rooms/${props.id}?checkIn=${props.checkIn}&checkOut=${props.checkOut}`}>Виж Още</Link>
+                ) : (
+                    <Link to={`/rooms/${props.id}`}>Виж Още</Link>
+                )
+            }
         </div>
     )
 }
